8f0959173a1b56dce3519d184ca8b5bfd8a0243e
Your Name <rghzaiel@ggn-group.com>
2023-07-31 12:31:19 +0100

    handle error

--- src/pages/SuperHeroes.js
+++ src/pages/SuperHeroes.js
@@ -4,16 +4,26 @@ import { useEffect, useState } from "react";
 const SuperHeroes = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
-    axios.get("http://localhost:4000/superheroes").then((res) => {
-      setData(res.data);
-      setIsLoading(false);
-    });
+    axios
+      .get("http://localhost:4000/superheroes")
+      .then((res) => {
+        setData(res.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsLoading(false);
+      });
   }, []);
 
   if (isLoading) {
     return <h2>Loading...</h2>;
+  }
+  if (error) {
+    return <h2>{error}</h2>;
   }
 
   return (
